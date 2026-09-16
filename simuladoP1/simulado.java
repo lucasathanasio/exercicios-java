9e6758f5936798b3effb7d0939d54cb940d5b72e
Lucas Athanasio <lucasbuenodeandrade@gmail.com>
2026-09-16 16:37:43 -0300

    resolução atualizada

--- simuladoP1/simulado.java
+++ simuladoP1/simulado.java
@@ -1,33 +1,94 @@
 public class Complexo {
+
+    // a) Estruturas internas necessárias
     private double a;
     private double b;
 
-    public Complexo(double a, double b) {
+    // b) Construtor com validacao em ponto flutuante
+    public Complexo(double a, double b) throws Exception {
+        if (a - a != 0.0) throw new Exception("Valor inválido");
         this.a = a;
+
+        if (b - b != 0.0) throw new Exception("Valor inválido");
         this.b = b;
     }
 
-    public Complexo mais(Complexo outro) throws Exception {
-        return new Complexo(, )
+    // c) Método mais: soma this com novo
+    public Complexo mais(Complexo novo) throws Exception {
+        if (novo == null) throw new Exception("Valor inválido");
+
+        double novoA = this.a + novo.a;
+        double novoB = this.b + novo.b;
+
+        return new Complexo(novoA, novoB);
     }
 
-    public Complexo menos(Complexo outro) throws Exception {
-        outro = 0;
-        outro = this.a - this.b;
-        return new Complexo(, )
-    }
-    
-    public Complexo vezes(Complexo outro) throws Exception {
-        outro = 0;
-        outro = this.a * this.b;
-        return new Complexo(, )
+    // d) Método menos: subtracao de this por novo
+    public Complexo menos(Complexo novo) throws Exception {
+        if (novo == null) throw new Exception("Valor inválido");
+
+        double novoA = this.a - novo.a;
+        double novoB = this.b - novo.b;
+
+        return new Complexo(novoA, novoB);
     }
 
-    public Complexo divididoPor(Complexo outro) throws Exception {
-        if (b > a) throw new Exception("o valor de b não pode ser maior que o valor de a");
-        outro = 0;
-        outro = this.a / this.b;
-        return new Complexo(, )
+    // e) Método vezes: multiplicacao de this por novo
+    public Complexo vezes(Complexo novo) throws Exception {
+        if(novo == null) throw new Exception("Valor inválido");
+
+        double novoA = (this.a * novo.a) - (this.b * novo.b);
+        double novoB = (this.a * novo.b) + (novo.a * this.b);
+
+        return new Complexo(novoA, novoB);
     }
 
+    // f) Método divididoPor: divisao de this por novo
+    public Complexo divididoPor(Complexo novo) throws Exception {
+        if (novo == null) throw new Exception("Valor inválido");
+
+        double denominador = (novo.a * novo.a) + (novo.b * novo.b);
+        if (denominador == 0.0) throw new Exception ("Divisao por zero");
+
+        double novoA = (this.a * novo.a + this.b * novo.b) / demoninador;
+        double novoB = (novo.a * this.b - this.a * novo.b) / demoninador;
+
+        return new Complexo(novoA, novoB);
+    }
+
+    // g) Métodos obrigatórios aplicáveis (apenas toString, equals e hashCode)
+    @Override
+    public String toString()
+    {
+        if (this.b == 0.0) return "" + this.a;
+        if (this.a == 0.0) return this.b + "i";
+        if (this.b < 0.0)  return this.a + " - " + (-this.b) + "i";
+        return this.a + " + " + this.b + "i";
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == this) return true;
+        if (obj == null) return false;
+        if (obj.getClass() != this.getClass()) return false;
+
+        Complexo complexo = (Complexo)obj;
+        if (Double.compare(this.a, complexo.a) != 0) return false;
+        if (Double.compare(this.b, complexo.b) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int retorno = 1; /* um nº natural qualquer, menos o zero */
+
+        retorno = retorno * 2 /* um nº primo qualquer */ + ((Double)this.a).hashCode();
+        retorno = retorno * 2 /* um nº primo qualquer */ + ((Double)this.b).hashCode();
+
+        if (retorno < 0) retorno = -retorno;
+        return retorno;
+    }
 }
